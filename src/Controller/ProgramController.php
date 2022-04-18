129c101d6df93e338d254d36e8788a62554931ea
geraldglatt <geraldglatt89@gmail.com>
2022-04-18 19:41:41 +0200

    Add controller class

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/program', name:'program_')]
+class ProgramController extends AbstractController
+{
+    #[Route('/', name:'index')]
+    public function index():Response
+    {
+        
+        return $this->render('program/index.html.twig', [
+            'website' => "website",
+        ]);
+    }
+    #[Route('/{id<\d+>}', methods:['GET'], name:'show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
+}
